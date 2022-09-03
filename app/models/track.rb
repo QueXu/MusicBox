0bd5b91c694f48826db6994f4d0321dc18363caf
QueXu <dmytro.kachmar.tr.2020@lpnu.ua>
2022-09-03 20:33:05 +0300

    added validations

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,7 +1,19 @@
 class Track < ApplicationRecord
 
     has_one_attached :audio, dependent: :destroy
+
     has_one_attached :image, dependent: :destroy
 
+    validates_presence_of :song, on: :create, message: "can't be blank"
+
+    validates_presence_of :artist, on: :create, message: "can't be blank"
+
+    validates_presence_of :audio, on: :create, message: "can't be blank"
+
+    validates_presence_of :image, on: :create, message: "can't be blank"
+
+    validates :song, length: { maximum: 40 }
+
+    validates :artist, length: { maximum: 20 }
 
 end
